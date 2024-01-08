9fab5c07e97f96b8ed0a25105d0cd8a03db43bd2
Swarna Jang Maden <limbujungmaden@gmail.com>
2024-01-08 17:08:00 +0545

    add createAt and deletedAt

--- App.jsx
+++ App.jsx
@@ -14,6 +14,8 @@ export default function App() {
        const currentNote =
               notes.find((note) => note.id === currentNoteId) || notes[0];
 
+       const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt);
+
        React.useEffect(() => {
               const unsubscribe = onSnapshot(
                      notesCollection,
@@ -39,6 +41,8 @@ export default function App() {
        async function createNewNote() {
               const newNote = {
                      body: "# Type your markdown note's title here",
+                     createdAt: Date.now(),
+                     updatedAt: Date.now(),
               };
               const newNoteRef = await addDoc(notesCollection, newNote);
               setCurrentNoteId(newNoteRef.id);
@@ -46,7 +50,11 @@ export default function App() {
 
        async function updateNote(text) {
               const docRef = doc(db, "notes", currentNoteId);
-              await setDoc(docRef, { body: text }, { merge: true });
+              await setDoc(
+                     docRef,
+                     { body: text, updatedAt: Date.now() },
+                     { merge: true }
+              );
        }
 
        async function deleteNote(noteId) {
@@ -63,7 +71,7 @@ export default function App() {
                                    className="split"
                             >
                                    <Sidebar
-                                          notes={notes}
+                                          notes={sortedNotes}
                                           currentNote={currentNote}
                                           setCurrentNoteId={setCurrentNoteId}
                                           newNote={createNewNote}
